4fd8e59f1221674f734bd767baa8b2a3d5a5dbf3
Srikar <150589840+Ignite7871@users.noreply.github.com>
2025-08-14 14:00:42 +0530

    Add files via upload

---
+++ storz/src/main/java/com/storz/storz/StorzApplication.java
@@ -0,0 +1,13 @@
+package com.storz.storz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorzApplication.class, args);
+	}
+
+}
---
+++ storz/src/test/java/com/storz/storz/StorzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storz.storz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
